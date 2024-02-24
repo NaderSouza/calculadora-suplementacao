37e1a1f5474d3166b020b0fb4a360386b3e84c48
NaderSouza <naderhadadsouza@gmail.com>
2024-02-24 19:10:21 -0300

    fix: Add svg icons in labels and change some components, try to create a dropdown menu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,31 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Fira_Code } from "next/font/google";
+import { Select, Option } from "@material-tailwind/react";
+import Link from "next/link";
+import { TESelect } from "tw-elements-react";
+
+import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
-
+const Selector = () => {
+  return (
+    <div className=" w-72 font-medium h-80">
+      <div className="bg-white w-full"></div>
+    </div>
+  );
+};
 export default function Home() {
   return (
+    //  TELA TODA
+
     <main
-      className={`min-h-screen min-w-screen bg-slate-950 ${inter.className}`}
+      className={`min-h-screen min-w-screen w-screen h-screen bg-slate-950 ${inter.className}`}
     >
-      <section className="max-w-screen-xl m-auto pt-20 bg-slate-950">
+      {/* CONTAINER DO FORM */}
+      <section className="max-w-screen-xl  m-auto pt-20 bg-slate-950">
         <div className="">
+          {/* IMG FORM */}
           <div className="flex flex-col items-center text-center sm:mx-auto sm:w-full sm:max-w-sm">
             <Image
               width={100}
@@ -19,7 +34,9 @@ export default function Home() {
               alt=""
               className="max-w-[100px]"
             />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+
+            {/* TEXTO DA CALCULADORA */}
+            <h2 className="mb-5 text-center text-2xl font-bold leading-9 tracking-tight text-white">
               Calculadora de suplementação
             </h2>
           </div>
@@ -27,52 +44,87 @@ export default function Home() {
         <div className="flex flex-col items-center text-center sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
             <div>
+              {/* TEXTO DO PESO */}
               <label
-                className="block text-sm font-medium leading-6 text-gray-300"
-                id="texto-peso"
+                htmlFor="input-6"
+                className="block text-sm font-medium text-gray-700 dark:text-gray-100"
               >
                 Peso
               </label>
-              <div className="mt-2">
+
+              {/* INPUT DO PESO */}
+              <div className="relative mt-1">
                 <input
-                  id="input-peso"
-                  name="peso"
                   type="number"
-                  autoComplete="number"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-200 sm:text-sm sm:leading-6"
+                  id="input-6"
+                  className="block w-full h-10 pl-8 pr-3 mt-1 text-sm text-gray-700 border focus:outline-none rounded shadow-sm focus:border-blue-500"
+                  placeholder="Digite seu peso"
                 />
+
+                {/* ICON DO INPUT */}
+                <span className="absolute inset-y-0 left-0 flex items-center justify-center ml-2">
+                  <Image
+                    width={100}
+                    height={100}
+                    src="/peso.svg"
+                    alt=""
+                    className="w-4 h-4 text-blue-400 pointer-events-none"
+                  />
+
+                  {/* Ainda testar esse SVG ---
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    className="w-4 h-4 text-blue-400 pointer-events-none"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                    ></path>
+                  </svg> */}
+                </span>
               </div>
             </div>
-
             <div>
-              <div className="flex items-center justify-between">
-                <label
-                  className="block text-sm font-medium leading-6 text-gray-300"
-                  id="texto-gordura"
-                >
-                  Porcentagem de gordura corporal
-                </label>
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
-                  ></a>
-                </div>
-              </div>
-              <div className="mt-2">
+              {/* TEXTO DA GORDURA */}
+              <label
+                htmlFor="input-6"
+                className="block text-sm font-medium text-gray-700 dark:text-gray-100"
+              >
+                Porcentagem de gordura corporal
+              </label>
+              {/* INPUT DA GORDURA */}
+              <div className="relative mt-1">
                 <input
-                  id="input-gordura"
-                  name="gordura"
                   type="number"
-                  autoComplete="number"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  id="input-6"
+                  className="block w-full h-10 pl-8 pr-3 mt-1 text-sm text-gray-700 border focus:outline-none rounded shadow-sm focus:border-blue-500"
+                  placeholder="Digite sua % de gordura"
                 />
+
+                {/* ICON DA GORDURA */}
+                <span className="absolute inset-y-0 left-0 flex items-center justify-center ml-2">
+                  <Image
+                    width={100}
+                    height={100}
+                    src="/gordura.svg"
+                    alt=""
+                    className="w-4 h-4 text-blue-400 pointer-events-none"
+                  />
+                </span>
               </div>
             </div>
+            {/* BTN DROPDOWN */}
 
+            {/* FINAL DO DROPDOWN */}
+
+            {/* BTN DE CALCULAR */}
             <div>
+              <Link rel="stylesheet" href="/src/pages/teste.tsx" />
               <button
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-fuchsia-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
