d42c137276739c7cc0eb879f20545827f836f93b
NaderSouza <naderhadadsouza@gmail.com>
2024-02-26 13:09:07 -0300

    fix: Fix the button color when pass the mouse and fix the radio buttons

--- src/pages/components/Button.tsx
+++ src/pages/components/Button.tsx
@@ -9,9 +9,7 @@ export function Button({ success = false, ...props }: ButtonProps) {
   return (
     <button
       data-success={success}
-      className={`rounded px-4 h-9 text-sm font-medium text-zinc-900 hover:bg-zinc-200 ${
-        success ? "bg-emerald-500 hover:bg-emerald-600" : ""
-      }`}
+      className={`rounded px-4 h-9 text-sm font-medium text-zinc-900 hover:bg-zinc-200 data-[success=true]:bg-emerald-500 data-[success=true]:hover:bg-emerald-600`}
       {...props}
     >
       {props.children}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -120,7 +120,7 @@ export default function Home() {
                     className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-gray-400 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-400 checked:before:bg-gray-400 hover:before:opacity-10"
                     id="html"
                   />
-                  <span className="absolute text-gray-500 transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+                  <span className="absolute text-emerald-500 transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       className="h-3.5 w-3.5"
@@ -133,9 +133,9 @@ export default function Home() {
                 </label>
                 <label
                   className="mt-px font-light text-gray-200 cursor-pointer select-none"
-                  htmlFor="html"
+                  htmlFor="growth"
                 >
-                  HTML 2
+                  Growth
                 </label>
               </div>
               <div className="inline-flex items-center">
@@ -150,7 +150,37 @@ export default function Home() {
                     id="react"
                     checked
                   />
-                  <span className="absolute text-gray-500 transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+                  <span className="absolute text-emerald-500 transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-3.5 w-3.5"
+                      viewBox="0 0 16 16"
+                      fill="currentColor"
+                    >
+                      <circle data-name="ellipse" cx="8" cy="8" r="8"></circle>
+                    </svg>
+                  </span>
+                </label>
+                <label
+                  className="mt-px font-light text-gray-200 cursor-pointer select-none"
+                  htmlFor="max_titanium"
+                >
+                  Max Titanium
+                </label>
+              </div>
+              <div className="inline-flex items-center">
+                <label
+                  className="relative flex items-center p-3 rounded-full cursor-pointer"
+                  htmlFor="react"
+                >
+                  <input
+                    name="type"
+                    type="radio"
+                    className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-gray-400 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-400 checked:before:bg-gray-400 hover:before:opacity-10"
+                    id="integral_médica"
+                    checked
+                  />
+                  <span className="absolute text-emerald-500 transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       className="h-3.5 w-3.5"
@@ -165,7 +195,7 @@ export default function Home() {
                   className="mt-px font-light text-gray-200 cursor-pointer select-none"
                   htmlFor="react"
                 >
-                  React
+                  Integral Médica
                 </label>
               </div>
             </div>
