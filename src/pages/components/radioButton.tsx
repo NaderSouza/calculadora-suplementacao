4f41a0fed5e54ef017a5a3418c4e93a1d9f52c33
NaderSouza <naderhadadsouza@gmail.com>
2024-02-29 23:59:11 -0300

    fix: fix vercel and radioButton, click in label and works

--- src/pages/components/radioButton.tsx
+++ src/pages/components/radioButton.tsx
@@ -13,13 +13,13 @@ export default function RadioButton({ value, label }: OptionProps) {
     <div className="inline-flex items-center mt-3">
       <label
         className="relative flex items-center p-3 rounded-full cursor-pointer"
-        htmlFor="html"
+        htmlFor={`radioButton-${value}`}
       >
         <input
           name="type"
           type="radio"
           className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-gray-400 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-400 checked:before:bg-gray-400 hover:before:opacity-10"
-          id="html"
+          id={`radioButton-${value}`}
           value={value}
         />
         <span className="absolute text-emerald-500 transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
@@ -35,7 +35,7 @@ export default function RadioButton({ value, label }: OptionProps) {
       </label>
       <label
         className="mt-px text-gray-200 cursor-pointer select-none"
-        htmlFor={label}
+        htmlFor={`radioButton-${value}`}
         onClick={handleLabelClick}
       >
         {label}
