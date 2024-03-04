c84f385cff4126824221b06b432f5df61b6e388b
NaderSouza <naderhadadsouza@gmail.com>
2024-03-03 21:46:18 -0300

    fix: fixed the modal calc

--- src/pages/components/Modal.tsx
+++ src/pages/components/Modal.tsx
@@ -59,13 +59,12 @@ export function ModalCalc({
   title,
   children,
   buttonText,
-
   onClose,
 }: ModalProps) {
   return (
     <>
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-        <div className="relative my-6 mx-auto w-2/3 max-w-3xl">
+        <div className="relative my-6 mx-auto w-full max-w-6xl">
           <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-slate-800 outline-none focus:outline-none">
             <div className=" items-start justify-between p-3 border-b border-solid border-slate-500 rounded-t">
               <h3 className="text-3xl font-semibold text-center">
