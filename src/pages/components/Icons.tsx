fc7f6aa3ef87fa356b70d194a4de9b04384e5782
NaderSouza <naderhadadsouza@gmail.com>
2024-03-03 21:39:13 -0300

    fix: added forget save files from icon

--- src/pages/components/Icons.tsx
+++ src/pages/components/Icons.tsx
@@ -1,6 +1,19 @@
+import React from "react";
 import { AlertTriangle, CheckCircle } from "lucide-react";
 
 export const icons = {
   warning: <AlertTriangle className="text-yellow-400 size-12 pb-2 m-auto" />,
   success: <CheckCircle className="text-emerald-500 size-12 pb-2 m-auto" />,
 };
+
+interface IconProps {
+  name: keyof typeof icons;
+}
+
+const Icon: React.FC<IconProps> = ({ name }) => {
+  const selectedIcon = icons[name] || null;
+
+  return <div className="inline-block">{selectedIcon}</div>;
+};
+
+export default Icon;
