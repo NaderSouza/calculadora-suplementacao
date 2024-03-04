169c1ce3457b26cb20a3d1c40c92d617326eaa16
Luiz Ricardo Soares dos Santos <luizr726@gmail.com>
2024-03-03 22:32:41 -0300

    fix: unification modals

--- @types/Components.ts
+++ @types/Components.ts
@@ -8,38 +8,38 @@ type ModalProps = {
 	title: string;
 	children: any;
 	buttonText: string;
-	icon: 'warning' | 'success';
-	onClose?: () => void;
+	icon: "warning" | "success";
+	onClose: (value?: boolean) => any;
 };
 
-type Brand = 'growth' | 'maxTitanium' | 'integralMedica';
+type Brand = "growth" | "maxTitanium" | "integralMedica";
 
 type BodyData = {
 	weight: number;
 	bodyFatPercentage: number;
-	brand: Brand
+	brand: Brand;
 };
 
 type Table = {
 	columns: string[];
 	rows: string[][];
-}
+};
 
 type Scoops = {
 	protein: number;
 	creatineSaturation: number;
 	creatineMaintenance: number;
 	betaAlanine: number;
-}
+};
 
 type Dosages = {
 	protein: number;
 	creatineSaturation: number;
 	creatineMaintenance: number;
 	betaAlanine: number;
-}
+};
 
 type ResultSuplements = {
 	dosages: Dosages;
 	scoops: Scoops;
-}
+};
