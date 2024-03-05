c6f78bd36beb0c82fb80b47691c0cd27d223e78c
NaderSouza <naderhadadsouza@gmail.com>
2024-03-05 14:43:11 -0300

    Updates

---
+++ server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{"/":["static/media/328468c522d8f1fe-s.p.woff2"]},"app":{},"appUsingSizeAdjust":false,"pagesUsingSizeAdjust":true}';
---
+++ static/chunks/pages/components/Icons-d909922beca6e382.js
@@ -0,0 +1,16 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[507],{4297:function(e,t,r){"use strict";r.d(t,{Z:function(){return o}});var n=r(7294),s={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"};/**
+ * @license lucide-react v0.340.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let i=e=>e.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase().trim(),o=(e,t)=>{let r=(0,n.forwardRef)(({color:r="currentColor",size:o=24,strokeWidth:u=2,absoluteStrokeWidth:a,className:c="",children:l,...d},h)=>(0,n.createElement)("svg",{ref:h,...s,width:o,height:o,stroke:r,strokeWidth:a?24*Number(u)/Number(o):u,className:["lucide",`lucide-${i(e)}`,c].join(" "),...d},[...t.map(([e,t])=>(0,n.createElement)(e,t)),...Array.isArray(l)?l:[l]]));return r.displayName=`${e}`,r}},1853:function(e,t,r){"use strict";r.d(t,{Z:function(){return n}});/**
+ * @license lucide-react v0.340.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let n=(0,r(4297).Z)("AlertTriangle",[["path",{d:"m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z",key:"c3ski4"}],["path",{d:"M12 9v4",key:"juzpu7"}],["path",{d:"M12 17h.01",key:"p32p05"}]])},3756:function(e,t,r){"use strict";r.d(t,{Z:function(){return n}});/**
+ * @license lucide-react v0.340.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let n=(0,r(4297).Z)("CheckCircle",[["path",{d:"M22 11.08V12a10 10 0 1 1-5.93-9.14",key:"g774vq"}],["path",{d:"m9 11 3 3L22 4",key:"1pflzl"}]])},5635:function(e,t,r){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Icons",function(){return r(8350)}])},8350:function(e,t,r){"use strict";r.r(t);var n=r(5893),s=r(1853),i=r(3756);t.default={warning:(0,n.jsx)(s.Z,{className:"text-yellow-400 size-12 pb-2 m-auto"}),success:(0,n.jsx)(i.Z,{className:"text-emerald-500 size-12 pb-2 m-auto"})}}},function(e){e.O(0,[888,774,179],function(){return e(e.s=5635)}),_N_E=e.O()}]);
