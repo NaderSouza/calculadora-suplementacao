4b69703a102e0e9bc17fa722bed19a13af07e767
NaderSouza <naderhadadsouza@gmail.com>
2024-03-06 21:21:38 -0300

    Updates

---
+++ static/chunks/pages/components/Icons-61888fe54dfbcbc7.js
@@ -0,0 +1,16 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[507],{4297:function(e,t,n){"use strict";n.d(t,{Z:function(){return o}});var r=n(7294),u={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"};/**
+ * @license lucide-react v0.340.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let i=e=>e.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase().trim(),o=(e,t)=>{let n=(0,r.forwardRef)(({color:n="currentColor",size:o=24,strokeWidth:s=2,absoluteStrokeWidth:c,className:a="",children:l,...d},h)=>(0,r.createElement)("svg",{ref:h,...u,width:o,height:o,stroke:n,strokeWidth:c?24*Number(s)/Number(o):s,className:["lucide",`lucide-${i(e)}`,a].join(" "),...d},[...t.map(([e,t])=>(0,r.createElement)(e,t)),...Array.isArray(l)?l:[l]]));return n.displayName=`${e}`,n}},1853:function(e,t,n){"use strict";n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.340.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(4297).Z)("AlertTriangle",[["path",{d:"m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z",key:"c3ski4"}],["path",{d:"M12 9v4",key:"juzpu7"}],["path",{d:"M12 17h.01",key:"p32p05"}]])},3756:function(e,t,n){"use strict";n.d(t,{Z:function(){return r}});/**
+ * @license lucide-react v0.340.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,n(4297).Z)("CheckCircle",[["path",{d:"M22 11.08V12a10 10 0 1 1-5.93-9.14",key:"g774vq"}],["path",{d:"m9 11 3 3L22 4",key:"1pflzl"}]])},5635:function(e,t,n){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Icons",function(){return n(8350)}])},8350:function(e,t,n){"use strict";n.r(t),n.d(t,{default:function(){return o}});var r=n(5893),u=n(1853),i=n(3756);function o(e){let{type:t}=e;return({warning:(0,r.jsx)(u.Z,{className:"text-yellow-400 size-12 pb-2 m-auto"}),success:(0,r.jsx)(i.Z,{className:"text-emerald-500 size-12 pb-2 m-auto"})})[t]}}},function(e){e.O(0,[888,774,179],function(){return e(e.s=5635)}),_N_E=e.O()}]);
